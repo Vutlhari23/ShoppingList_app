1202989271cb4a9aa0d190f48c9a9c5b466ad330
Vutlhari23 <vutlhari23m@gamil.com>
2026-08-25 14:16:17 +0200

    Ops : Add ContentContainer  component

---
+++ src/components/ContentContainer/ContentContainer.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+type ContainerProp= {
+    className?: string;
+    style?: React.CSSProperties;
+    children:React.ReactNode
+}
+
+export const ContentContainer = ({className,style,children}: ContainerProp) => {
+  return (
+    <div  className={className} style={style}>
+        {children}
+    </div>
+  )
+}
+
