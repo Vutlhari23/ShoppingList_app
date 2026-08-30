de9c5c73298e892f4b42749d17eea86860520c3e
Vutlhari23 <vutlhari23m@gamil.com>
2026-08-30 22:08:40 +0200

    Style : Add style for TextInput

--- src/components/TextInput/TextInput.tsx
+++ src/components/TextInput/TextInput.tsx
@@ -1,39 +1,6 @@
-import styles from './TextInput.module.css'
+import type { InputHTMLAttributes } from "react";
+import styles from "./TextInput.module.css";
 
-type InputTextProps = {
-    type?: string,
-    id?: string,
-    placeholder?: string,
-    value?: string,
-    onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
-    style?: React.CSSProperties,
-    className?: string,
-    label?: string,
-    error?: string,
-    name?: string,
-    disabled?: boolean
-    
-}
-
-export const TextInput = ({ id, placeholder, onChange, value, style, className, label, error, name, type = "text", disabled = false }: InputTextProps) => {
-
-    const inputClass = `${styles.input} ${className || ''}`.trim();
-
-    return (
-        <div className={styles.inputGroup}>
-            {label && <label className={styles.label} htmlFor={id}>{label}</label>}
-            <input
-                type={type}
-                id={id}
-                name={name}
-                placeholder={placeholder}
-                value={value}
-                onChange={onChange}
-                style={style}
-                className={inputClass}
-                disabled={disabled}
-            />
-            {error && <span className={styles['input-error']}>{error}</span>}
-        </div>
-    )
-}
+export const TextInput = ({ className, ...rest }: InputHTMLAttributes<HTMLInputElement>) => (
+  <input className={`${styles.input} ${className ?? ""}`} {...rest} />
+);
